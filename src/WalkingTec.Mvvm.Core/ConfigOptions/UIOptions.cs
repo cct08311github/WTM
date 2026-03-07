1f1f4c7e34a17c29be2b7e19c679141823d5acb1
cct0831 <cct0831github@gmail.com>
2026-03-07 09:52:53 +0800

    refactor: enable nullable in ui options

--- src/WalkingTec.Mvvm.Core/ConfigOptions/UIOptions.cs
+++ src/WalkingTec.Mvvm.Core/ConfigOptions/UIOptions.cs
@@ -1,19 +1,19 @@
-#nullable disable
+#nullable enable
 namespace WalkingTec.Mvvm.Core.ConfigOptions
 {
     public class UIOptions
     {
-        public DataTableOptions DataTable { get; set; }
-        public ComboBoxOptions ComboBox { get; set; }
-        public DateTimeOptions DateTime { get; set; }
-        public SearchPanelOptions SearchPanel { get; set; }
+        public DataTableOptions DataTable { get; set; } = new DataTableOptions();
+        public ComboBoxOptions ComboBox { get; set; } = new ComboBoxOptions();
+        public DateTimeOptions DateTime { get; set; } = new DateTimeOptions();
+        public SearchPanelOptions SearchPanel { get; set; } = new SearchPanelOptions();
 
         public class DataTableOptions
         {
             /// <summary>
             /// 默认列表行数
             /// </summary>
-            public int RPP { get; set; }
+            public int RPP { get; set; } = DefaultConfigConsts.DEFAULT_RPP;
 
             public bool ShowPrint { get; set; }
 
@@ -26,7 +26,7 @@ namespace WalkingTec.Mvvm.Core.ConfigOptions
             /// <summary>
             /// 默认允许ComboBox搜索
             /// </summary>
-            public bool DefaultEnableSearch { get; set; }
+            public bool DefaultEnableSearch { get; set; } = DefaultConfigConsts.DEFAULT_COMBOBOX_DEFAULT_ENABLE_SEARCH;
         }
 
         public class DateTimeOptions
@@ -35,7 +35,7 @@ namespace WalkingTec.Mvvm.Core.ConfigOptions
             /// <summary>
             /// 默认开启DateTime只读
             /// </summary>
-            public bool DefaultReadonly { get; set; }
+            public bool DefaultReadonly { get; set; } = DefaultConfigConsts.DEFAULT_DATETIME_DEFAULT_READONLY;
         }
 
         public class SearchPanelOptions
@@ -44,7 +44,7 @@ namespace WalkingTec.Mvvm.Core.ConfigOptions
             /// <summary>
             /// 默认展开SearchPanel内容
             /// </summary>
-            public bool DefaultExpand { get; set; }
+            public bool DefaultExpand { get; set; } = DefaultConfigConsts.DEFAULT_SEARCHPANEL_DEFAULT_EXPAND;
         }
     }
 }
