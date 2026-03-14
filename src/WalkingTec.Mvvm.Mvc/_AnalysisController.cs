050093bfa6e69fd14177ba9ccaefe0f29d65e61f
cct0831 <cct0831github@gmail.com>
2026-03-14 23:21:50 +0800

    fix(analysis): add missing System.Reflection using for GetCustomAttribute

--- src/WalkingTec.Mvvm.Mvc/_AnalysisController.cs
+++ src/WalkingTec.Mvvm.Mvc/_AnalysisController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
