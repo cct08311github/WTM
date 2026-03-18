a8fbbf9a1a8e1c9a6ade34c1554a92bb0fa3bc7e
cct08311github <145902126+cct08311github@users.noreply.github.com>
2026-03-18 17:07:15 +0800

    fix(core): remove deprecated System.Data.SqlClient using from BaseImportVM (#572)

--- src/WalkingTec.Mvvm.Core/BaseImportVM.cs
+++ src/WalkingTec.Mvvm.Core/BaseImportVM.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
-using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
